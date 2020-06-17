9eec1580d63d0b02a7c5c287da0f17e619140a8f
Kevin Soria <60301136+kevin-soria@users.noreply.github.com>
2020-06-17 16:42:44 -0400

    second commit starting app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => {
-  material app
-
-
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[500],
+        appBar: AppBar(
+          title: Text("Sneakers Den"),
+          backgroundColor: Colors.orange[200],
+        ),
+      ),
+    ),
+  );
 }
