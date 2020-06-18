42808eb85288b33c2e4c84f0f43fd49fe246d316
Kevin Soria <60301136+kevin-soria@users.noreply.github.com>
2020-06-18 16:13:06 -0400

    bottom footer added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey[500],
-        appBar: AppBar(
-          title: Text("Sneakers Den"),
-          backgroundColor: Colors.orange[400],
-        ),
-        body: Center(
-          child: Image(
-            image: AssetImage('images/snkrs-logo-blacks.png'),
-          ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.grey[500],
+      appBar: AppBar(
+        title: Text("Sneakers Den"),
+        backgroundColor: Colors.orange[400],
+      ),
+      body: Image(
+        image: NetworkImage(
+          'https://www.snkrsden.com/images/pic.jpg',
+//          center(child: Image(
+//            image: AssetImage('images/snkrs-logo-blacks.png'),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Popular'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Search'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            title: Text('Dens'),
+          ),
+        ],
+      ),
     ),
-  );
+  ));
 }
