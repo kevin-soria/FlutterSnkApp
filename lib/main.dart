c309994ab968f3cc8499da7bde27e2977486beeb
Kevin Soria <60301136+kevin-soria@users.noreply.github.com>
2020-06-18 16:58:00 -0400

    container added but emm not working

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.grey[500],
-      appBar: AppBar(
-        title: Text("Sneakers Den"),
-        backgroundColor: Colors.orange[400],
-      ),
-      body: Image(
-        image: NetworkImage(
-          'https://www.snkrsden.com/images/pic.jpg',
-//          center(child: Image(
-//            image: AssetImage('images/snkrs-logo-blacks.png'),
+    home: Stack(// <-- STACK AS THE SCAFFOLD PARENT
+        children: [
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://www.snkrsden.com/images/pic.jpg'), // <-- BACKGROUND IMAGE
+            fit: BoxFit.cover,
+          ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.grade),
-            title: Text('Popular'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.grid_on),
-            title: Text('Search'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.group),
-            title: Text('Dens'),
-          ),
-        ],
+      Scaffold(
+        backgroundColor: Colors.grey[500],
+        appBar: AppBar(
+          title: Text("Sneakers Den"),
+          backgroundColor: Colors.orange[400],
+        ),
+        body: Center(
+          child: Image(image: AssetImage('Images/snkrs-logo-blacks.png')),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.grade),
+              title: Text('Popular'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.grid_on),
+              title: Text('Search'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.group),
+              title: Text('Dens'),
+            ),
+          ],
+        ),
       ),
-    ),
+    ]),
   ));
 }
